8aeb6470fa8eb9da79681bbf6ebcaad94b38d924
riggina <oceaniariggina@gmail.com>
2023-10-05 17:41:23 +0800

    complete sign up page functionality

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
@@ -17,12 +18,14 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+const PORT = process.env.PORT || 5173;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
 
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
