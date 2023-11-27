7d139edd16459009aa48238deac6eb8edd04d2a0
riggina <oceaniariggina@gmail.com>
2023-11-28 01:48:55 +0800

    completed update user functionality

--- fe/src/redux/user/userSlice.js
+++ fe/src/redux/user/userSlice.js
@@ -21,10 +21,22 @@ const userSlice = createSlice({
         signInFailure: (state, action) => {
             state.error = action.payload,
             state.loading = false;   
-        }
+        },
+        updateUserStart: (state) => {
+            state.loading = true;
+        },
+        updateUserSuccess: (state, action) => {
+            state.currentUser = action.payload,
+            state.loading = false,
+            state.error = null;
+        },
+        updatedUserFailure: (state, action) => {
+            state.error = action.payload,
+            state.loading = false;   
+        },
     }
 })
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const { signInStart, signInSuccess, signInFailure, updateUserStart, updateUserSuccess, updatedUserFailure } = userSlice.actions;
 
 export default userSlice.reducer;
