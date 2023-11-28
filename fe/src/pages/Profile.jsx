5bfae8a4c1bb3c6793bb21dbf56a93e12cb44dec
riggina <oceaniariggina@gmail.com>
2023-11-29 00:57:11 +0800

    completed create listing page UI

--- fe/src/pages/Profile.jsx
+++ fe/src/pages/Profile.jsx
@@ -13,6 +13,7 @@ import { updateUserStart,
         signoutUserSuccess,
         signoutUserFailure} from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null)
@@ -149,6 +150,9 @@ export default function Profile() {
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-80'>
         {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link to={"/create-listing"} className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
+          Create Listing
+        </Link>
       </form>
 
       <div className='flex justify-between mt-2'>
