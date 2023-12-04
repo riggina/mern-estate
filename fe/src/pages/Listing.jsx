212157e6fee2ef7b12d5bde44c93809e88baaa48
riggina <oceaniariggina@gmail.com>
2023-12-04 13:39:29 +0800

    add contact landroad functionality to listing page

--- fe/src/pages/Listing.jsx
+++ fe/src/pages/Listing.jsx
@@ -3,7 +3,9 @@ import {useParams} from 'react-router-dom';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import SwiperCore from 'swiper';
 import {Navigation} from 'swiper/modules';
+import { useSelector } from 'react-redux';
 import 'swiper/css/bundle';
+import Contact from '../components/Contact';
 import {
     FaBath,
     FaBed,
@@ -20,6 +22,8 @@ export default function Listing() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [copied, setCopied] = useState(false);
+    const [contact, setContact] = useState(false);
+    const { currentUser } = useSelector((state) => state.user);
     useEffect(() => {
         const fetchListing = async () => {
             try {
@@ -42,10 +46,11 @@ export default function Listing() {
         };
         fetchListing();
       }, [params.listingId]);
+
   return (
     <main>
         {loading && <p className='text-center my-7'>Loading...</p>}
-        {error && <p className='text-center my-7 text-red-700'>Something went wrong</p>}
+        {error && (<p className='text-center my-7 text-red-700'>Something went wrong</p>)}
         {listing && !loading && !error && (
             <div>
                 <Swiper navigation>
@@ -124,6 +129,15 @@ export default function Listing() {
                             {listing.furnished ? 'Furnished' : 'Unfurnished'}
                         </li>
                     </ul>
+                    {currentUser && listing.userRef == currentUser._id && !contact && (
+                    <button
+                        onClick={() => setContact(true)}
+                        className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+                    >
+                        Contact landlord
+                    </button>
+                    )}
+                    {contact && <Contact listing={listing} />}
                 </div>
             </div>
         )}
